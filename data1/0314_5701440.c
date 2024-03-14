272474139fdcfa6f67cb4cea69d2c8e3b1d67515
권성철 <tjdcjf8298@naver.com>
2024-03-14 14:08:17 +0900

    5701440

--- data1/0314_5701440.c
+++ data1/0314_5701440.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(void) {
 	int sum = 0;
+	bool isPrime[101];
 
-	for (int i = 1; i <= 100; i++) {
-
+	isPrime[0] = false;
+	isPrime[1] = false;
+	for (int i = 2; i <= 100; i++) {
+		isPrime[i] = true;
 	}
-	
+
+	for (int i = 2; i <= 10; i++) {
+		if (isPrime[i]) {
+			for (int j = i * i; j <= 100; j += i) { 
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	for (int i = 0; i < 100; i++) {
+		if (isPrime[i]) {
+			sum += i;
+		}
+	}
+	printf("소수의 합 :%d \n", sum);
 	return 0;
 }
