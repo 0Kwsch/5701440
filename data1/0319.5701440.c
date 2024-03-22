916e3d84282d3618b7396991e263944936c7149c
권성철 <tjdcjf8298@naver.com>
2024-03-22 21:31:20 +0900

    0319 / 5701440

---
+++ data1/0319.5701440.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<time.h>
+
+long long factorial_iter(int a) {
+	long long result = 1;
+	for (int i = 1; i <= a; i++) {
+		result *= i; //result = result * i
+
+	}
+	return result;
+}
+long long factorial_rec(int a) {
+	long long result = 1;
+	for (int i = 1; i <= a; i++) {
+		result *= i; //result = result * i
+
+	}
+	return result;
+
+}
+int main(void) {
+	clock_t start, stop;
+	double diff;
+
+	start = clock(); // 측정시작
+
+	long long result_iter = factorial_iter(20);
+	printf("factorial_iter 20! = %lld\n", result_iter);
+	
+	long long result_rec = factorial_rec(20);
+	printf("factorial_iter 20! = %lld\n", result_rec);
+
+	stop = clock();
+	diff = (double)(stop - start) / CLOCKS_PER_SEC;
+	printf("수행시간 : %lf", diff);
+	return 0;
+}
--- data1/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void) {
-	int sum = 0;
-
-
-	printf("100 이하의 소수의 합:%d",sum);
-	return 0;
-}
