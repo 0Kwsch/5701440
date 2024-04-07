318e17d58b0f81d6cd5574ad537bc1a966e0ef58
권성철 <tjdcjf8298@naver.com>
2024-04-07 21:46:52 +0900

    0402.5701440

---
+++ 0402.5701440 2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAX_TERMS 101
+
+struct term
+{
+	float coef;
+	int expon;
+
+}terms[MAX_TERMS] = {
+	{7.0, 6}, {5.0, 3}, {9.0, 2}, {1.0, 0},
+	{5.0, 3}, {2.0, 2}, {1.0, 1}, {10.0, 0}
+};
+
+int avail = 7;
+
+int As = 0, Ae = 3, Bs = 4, Be = 7, Cs, Ce;
+
+void polymult(int as, int ae, int bs, int be, int* cs, int* ce) {
+	*cs = avail;
+
+
+	for (; as <= ae; as++) {
+		for (; bs <= be; bs++) {
+			terms[*cs].coef = terms[as].coef * terms[bs].coef;
+			terms[*cs].expon = terms[as].expon + terms[bs].expon;
+			(*cs)++;
+		}
+	}
+	*cs = avail;
+	*ce = *cs - 1;
+
+}
+
+void print_poly(int s, int e) {
+	for (int i = s; i < e; i++)
+		printf("%3.1fx^%d +", terms[i].coef, terms[i].expon);
+	printf("%3.1fx^%d  \n", terms[e].coef, terms[e].expon);
+}
+
+int main(void) {
+	polymult(As, Ae, Bs, Be, &Cs, &Ce);
+	print_poly(As, Ae);
+	print_poly(Bs, Be);
+	printf("------------------------------------\n");
+	print_poly(Cs, Ce);
+	return 0;
+
+}
--- data1/0319.5701440.c
+++ data1/0319.5701440.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<time.h>
 
-long long factorial_iter(int a) {
+/*long long factorial_iter(int a) {
 	long long result = 1;
 	for (int i = 1; i <= a; i++) {
 		result *= i; //result = result * i
@@ -14,7 +14,7 @@ long long factorial_rec(int a) {
 	else return (a * factorial_rec (a - 1));
 
 }
-int main(void) {
+int main(void) { 
 	clock_t start, stop;
 	double diff;
 
@@ -29,5 +29,11 @@ int main(void) {
 	stop = clock();
 	diff = (double)(stop - start) / CLOCKS_PER_SEC;
 	printf("수행시간 : %lf", diff);
+	return 0;
+}*/
+int main(void) {
+	for (int i = 1; i <= 5; i++) {
+		printf(" aoi ");
+	}
 	return 0;
 }
